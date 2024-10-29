186f27c36e115c940036b53dae129f0d6bf0a877
Sukrutha Shamsundar <Sukrutha2024@gmail.com>
2024-10-29 12:01:43 +0530

    simple maven project

---
+++ src/main/java/com/sukrutha/simplemaven/App.java
@@ -0,0 +1,14 @@
+package com.sukrutha.simplemaven;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		String message = "Hello Maven";
+
+		System.out.println(StringUtils.reverse(message));
+
+	}
+}
